6782e4896d64f3a35ff21d01ea8c1214abb7848e
blessymarybiju <138175146+blessymarybiju@users.noreply.github.com>
2024-08-30 19:38:58 +0530

    Update my_server_config.ps1

--- my_server_config.ps1
+++ my_server_config.ps1
@@ -1,8 +1,18 @@
-Configuration MyServerConfig {
-    Node "localhost" {
-        WindowsFeature "Web-Server" {
+Configuration SampleDSC {
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+    Node 'localhost' {
+        WindowsFeature InstallWebServer {
             Ensure = "Present"
             Name   = "Web-Server"
         }
+
+        File CreateSampleFile {
+            DestinationPath = "C:\\inetpub\\wwwroot\\index.html"
+            Contents        = "Hello, World!"
+        }
     }
 }
+
+SampleDSC
+Start-DscConfiguration -Path .\SampleDSC -Wait -Verbose
