72ee2e35a2bca9e545498970c8ed54530f0316ac
blessymarybiju <138175146+blessymarybiju@users.noreply.github.com>
2024-08-30 13:55:35 +0530

    Update rbac_policies.tf

--- rbac_policies.tf
+++ rbac_policies.tf
@@ -1,5 +1,1 @@
-resource "azurerm_role_assignment" "example" {
-  scope              = "/subscriptions/<your-subscription-id>/resourceGroups/<your-resource-group>"
-  role_definition_name = "Contributor"
-  principal_id       = "<service-principal-id>"
-}
+
