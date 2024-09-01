5e497ca7b2edf8cfbd1aeff2adf27407a58d269d
blessymarybiju <138175146+blessymarybiju@users.noreply.github.com>
2024-09-02 00:15:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,1 @@
-provider "azurerm" {
-  features {}
 
-  subscription_id = "997fc32b-74ea-408b-a24f-222f0d5d4c76"  
-  tenant_id       = "e7aba1a0-d43b-4a50-ba72-e9d0d3f8ea4b"       
-  client_id       = "d56cb605-b85c-404e-a990-c8b3b0247b32"         
-  client_secret   = "UrR8Q~nma6CyFQ4tlE5ZEX5JVUtwiI~ErO536bNz"  
-}
